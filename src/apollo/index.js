5e62c8823e31a5714db3e5949a1bbe4599fef5b8
Karlen Kishmiryan <karlenkishmiryan@gmail.com>
2019-01-31 02:02:13 +0400

    Implemented resolvers and schema for user.

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -1,14 +1,15 @@
 const { ApolloServer, gql } = require('apollo-server-express');
+const { userResolver, userSchema } = require('./user');
 
 module.exports = new ApolloServer({
   typeDefs: gql`
     type Query {
-      hello: String
+      users: [User]
+      user(id: ID!): User
     }
+    ${userSchema}
   `,
   resolvers: {
-    Query: {
-      hello: () => 'world'
-    }
+    ...userResolver
   },
 });
---
+++ src/apollo/user/index.js
@@ -0,0 +1,4 @@
+const userResolver = require('./user.resolver');
+const userSchema = require('./user.schema');
+
+module.exports = { userResolver, userSchema };
